d6c8e03e65cc4051950d62ca83072e59ca694dee
yusu <dbtnss@naver.com>
2023-05-11 23:39:50 +0900

    Feat: Add Site in Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://jangyusu.github.io/yusu.log',
   integrations: [mdx(), sitemap(), tailwind()],
 });
