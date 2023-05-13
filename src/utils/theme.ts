0689ba6ffaddc6d41d73fe50b7b92aa5fe77e632
yusu <dbtnss@naver.com>
2023-05-13 21:57:39 +0900

    Feat: 댓글 기능 추가
    - utterances 사용
    - Dynamic theme 적용

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,4 +1,16 @@
-import { getStorage } from '@utils/storage';
+import { getStorage, setStorage } from '@utils/storage';
+
+export const setTheme = (themeSwitch: Element) => {
+  themeSwitch.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark');
+    const theme = document.documentElement.classList.contains('dark')
+      ? 'dark'
+      : 'light';
+
+    setStorage('theme', theme);
+    setUtterancesTheme();
+  });
+};
 
 export const getTheme = () => {
   const storedTheme = getStorage('theme', '');
@@ -10,4 +22,23 @@ export const getTheme = () => {
   return window.matchMedia('(prefers-color-scheme: dark)').matches
     ? 'dark'
     : 'light';
+};
+
+export const setUtterancesTheme = () => {
+  const utterancesFrame = document.querySelector(
+    '.utterances-frame'
+  ) as HTMLIFrameElement;
+
+  if (!utterancesFrame) {
+    return;
+  }
+
+  const theme = getTheme();
+  const message = {
+    type: 'set-theme',
+    theme: `github-${theme}`,
+  };
+
+  utterancesFrame.contentWindow &&
+    utterancesFrame.contentWindow.postMessage(message, 'https://utteranc.es');
 };
