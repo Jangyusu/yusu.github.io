8c17b1413a46eb36cf361e90dbfaac50f2d15088
yusu <dbtnss@naver.com>
2023-05-22 22:52:26 +0900

    Style: Local Font 에서 Web Link Font 로 변경

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,16 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Pretendard', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
