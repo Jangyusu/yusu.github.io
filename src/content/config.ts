a0fb3772f290dd0c98938eb1afcce09f2fbf0868
yusu <dbtnss@naver.com>
2023-05-28 18:51:46 +0900

    Docs: Post id 속성 제거

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,6 @@ import { z, defineCollection } from 'astro:content';
 
 const postCollection = defineCollection({
   schema: z.object({
-    id: z.number(),
     title: z.string(),
     category: z.string(),
     createDate: z.date(),
