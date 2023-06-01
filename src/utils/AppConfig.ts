46b4c51c015010f5433600026b0b1570f37557e5
yusu <dbtnss@naver.com>
2023-06-02 00:25:41 +0900

    Refact: AppConfig 분리

---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  siteName: 'Yusu.log',
+  siteTitle: '유수의 기술 블로그',
+  siteDescription:
+    '안녕하세요! Yusu.log에 오신 것을 환영합니다. 저는 개발과 기술을 사랑하는 열정적인 개발자입니다. 이곳에서는 저의 개발 이야기와 기술적인 고민을 공유하고 있습니다.',
+};
